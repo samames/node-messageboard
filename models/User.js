404aebcd78fa8c39b2b19f0dd2fbaaa4c5b23580
sam ames <samames@gmail.com>
2020-10-20 01:54:53 +0100

    expire tokens after 30d

--- models/User.js
+++ models/User.js
@@ -111,7 +111,9 @@ userSchema.methods.toJSON = function () {
 
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
-  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
   user.tokens = user.tokens.concat({ token });
   await user.save();
   return token;
